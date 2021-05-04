c49467f415c549fa8d6d102117e9bd0370a63c62
Josep Seto <jsetojseto@gmail.com>
2021-05-04 14:18:07 +0200

    object values in selector

--- src/lib/malleable-select.tsx
+++ src/lib/malleable-select.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
 import { InputProps } from './maleable-input'
 
-interface Select__Props extends InputProps {
+interface MalleableSelectProps extends InputProps {
 	onChange: ( value: string ) => void
 	type: string
 }
 
-export function MalleableSelect( props: Select__Props ) {
+export function MalleableSelect( props: MalleableSelectProps ) {
 	const { label, values, defaultValue, className, onChange } = props
-	const [ value, setValue ] = useState( defaultValue as string || '' )
+	const defVal = parseValue( values.findIndex( val => parseValue( val ).label === defaultValue as string  ) ).label
+	const [ value, setValue ] = useState( defVal || '' )
 
 	return (
 		<div className={`malleable-ui multi-select ${ className || '' }`}>
@@ -19,16 +20,33 @@ export function MalleableSelect( props: Select__Props ) {
 				value={ value }
 				onChange={ event => {
 					setValue( event.target.value )
-					onChange( event.target.value )
+					onChange( values[ Number( event.target.value ) ] )
 				}}
 			>
 				{
-					values.map( value => (
-						<option key={ value } value={ value }>{ value }</option>
-					))
+					values.map( ( optionVal, idx ) => {
+						const val = parseValue( optionVal )
+						return (
+							<option key={ val.label } value={ idx }>{ val.label }</option>
+						)
+					})
 				}
 			</select>
 		</div>
 
 	)
+}
+
+function parseValue( value ) {
+	if( typeof( value ) !== 'object' ) {
+		return {
+			label: value,
+			value: value
+		}
+	} 
+	else {
+		return value
+	}
+
+
 }
